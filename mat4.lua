bb69e077c5968567770823e9a5e392c43d0fcbdc
navity <navi9209@gmail.com>
2015-07-08 14:11:16 +0200

    update

--- mat4.lua
+++ mat4.lua
@@ -3,9 +3,6 @@ local ffi = require("ffi")
 local vec3 = require("vec3")
 
 
-
-
-
 	local mat4 = {}
 
 function mat4:new(...)
@@ -74,6 +71,7 @@ end
 
 
 function mat4:translate(dest, vec)
+    local x,y,z = vec.x, vec.y, vec.z
   
 	if not dest then dest = mat4:new() end
 
@@ -89,14 +87,59 @@ function mat4:translate(dest, vec)
 	dest[9] = self[9]
 	dest[10] = self[10]
 	dest[11] = self[11]
-	dest[12] = self[0] * vec.x + self[4] * vec.y + self[8] * vec.z + self[12];
-	dest[13] = self[1] * vec.x + self[5] * vec.y + self[9] * vec.z + self[13];
-	dest[14] = self[2] * vec.x + self[6] * vec.y + self[10] * vec.z + self[14];
-	dest[15] = self[3] * vec.x + self[7] * vec.y + self[11] * vec.z + self[15];
+	dest[12] = self[0] * x + self[4] * y + self[8] * z + self[12];
+	dest[13] = self[1] * x + self[5] * y + self[9] * z + self[13];
+	dest[14] = self[2] * x + self[6] * y + self[10] * z + self[14];
+	dest[15] = self[3] * x + self[7] * y + self[11] * z + self[15];
 
 	return dest
 end
 
+function mat4:rotate(dest,angle,vec)
+    if not dest then dest = mat4:new() end
+    
+    local x,y,z = vec.x, vec.y, vec.z
+    
+    local c,s,t = math.cos(angle), math.sin(angle), 1 - c
+    
+    local   r11, r12, r13,
+            r21, r22, r23,
+            r31, r32, r33 =	x*x + (1-x)*c,  x*y*t-z*s,  x*z*t+s,
+				x*y*(1-c)+z*s,  y*y+(1-y*y)*c,  y*z*t-x*s,
+				x*z*t-y*s,      y*z*t+x*s,      z*z+(1-z*z)*c
+    
+    local	m11, m12, m13, m14,
+		m21, m22, m23, m24,
+		m31, m32, m33, m34,
+		m41, m42, m43, m44 =	self[0], self[1], self[2], self[3]
+					self[4], self[5], self[6], self[7]
+					self[8], self[9], self[10], self[11]
+					self[12], self[13], self[14], self[15]
+					
+					
+    dest[0] = m11 * r11 + m21 * r12 + m31 * r13
+    dest[1] = m12 * r11 + m22 * r12 + m32 * r13
+    dest[2] = m13 * r11 + m23 * r12 + m33 * r13
+    dest[3] = m14 * r11 + m24 * r12 + m34 * r13
+    
+    dest[4] = m11 * r21 + m21 * r22 + m31 * r23
+    dest[5] = m12 * r21 + m22 * r22 + m32 * r23
+    dest[6] = m13 * r21 + m23 * r22 + m33 * r23
+    dest[7] = m14 * r21 + m24 * r22 + m34 * r23
+    
+    dest[8] = m11 * r31 + m21 * r32 + m31 * r33
+    dest[9] = m12 * r31 + m22 * r32 + m32 * r33
+    dest[10] = m13 * r31 + m23 * r32 + m33 * r33
+    dest[11] = m14 * r31 + m24 * r32 + m34 * r33
+    
+
+    dest[12] = self[12]
+    dest[13] = self[13]
+    dest[14] = self[14]
+    dest[15] = self[15]
+
+    return dest
+end
 
 mat4.__mul = function(self, m) return self:mult(NULL,m) end
 
